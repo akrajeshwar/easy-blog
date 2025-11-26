59894d8bd47b4eaf38117d8530ca2e16d4db2e15
Rajeshwar Singh <146916986+akrajeshwar@users.noreply.github.com>
2025-11-26 19:23:19 +0530

    Set up Firebase and Firestore configuration

--- firebase.js
+++ firebase.js
@@ -1,1 +1,19 @@
+// Firebase imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, doc, getDoc } 
+  from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
+// Your Firebase config (paste your config)
+const firebaseConfig = {
+    apiKey: "AIzaSyCy8Ea1fKVorLQ0hVln_ELCVs3J1QOlOlw",
+    authDomain: "easyblog-c851b.firebaseapp.com",
+    projectId: "easyblog-c851b",
+    storageBucket: "easyblog-c851b.firebasestorage.app",
+    messagingSenderId: "616036452082",
+    appId: "1:616036452082:web:1a4a6f2348924a1680b26b"
+};
+
+// Initialize Firebase + Firestore
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export { collection, addDoc, getDocs, doc, getDoc };
